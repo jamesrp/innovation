180cb53e7f07c547c91cf6a8118e04cdc25353f6
James Pfeiffer <jamespfeiffer@jamess-mbp.lan>
2021-03-30 22:13:57 -0700

    Server mode.

--- lost-cities/src/App.js
+++ lost-cities/src/App.js
@@ -1,5 +1,5 @@
 import {Client} from 'boardgame.io/react';
-import {Local} from 'boardgame.io/multiplayer'
+import {SocketIO} from 'boardgame.io/multiplayer'
 import {LostCities} from './Game';
 import {LostCitiesBoard} from './Board';
 
@@ -7,16 +7,24 @@ import {LostCitiesBoard} from './Board';
 const LostCitiesClient = Client({
     game: LostCities,
     board: LostCitiesBoard,
-    multiplayer: Local(),
+    multiplayer: SocketIO({ server: 'localhost:8000' }),
     numPlayers: 2,
     debug: false,
 });
 
+// MakeClient routes a URL like:
+// localhost:3000/matchID/1
+// to matchID, seat 1.
+function MakeClient() {
+    let pathArray = window.location.pathname.split('/');
+    let player = pathArray[2];
+    let match = pathArray[1];
+    return <LostCitiesClient playerID={player} matchID={match}/>;
+}
+
 const App = () => (
     <div>
-        <LostCitiesClient playerID="0"/>
-        <hr/>
-        <LostCitiesClient playerID="1"/>
+        {MakeClient()}
     </div>
 );
 
